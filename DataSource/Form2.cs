f72c101f1556de2f1e4a8bf9c280183c792821f3
Leonardo Hernández <leonardo.hernandez@genesys.cl>
2021-11-25 13:08:17 -0500

    Initial commit

---
+++ DataSource/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace DataSource
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, System.EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void Form2_Load(object sender, System.EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwindDataSet.Customers' Puede moverla o quitarla según sea necesario.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
+    }
+}
